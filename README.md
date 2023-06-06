5a10652250f233e992aa48b42cd2072d6f863e66
Martin K <martyshkon@gmail.com>
2023-06-06 23:13:09 +0300

    Set up Discord bot instead

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Bracket Counter Revamped
-A second version of the BFDI/OSO bracket counter!
+A second version of the BFDI/OSO bracket counter, now as a Discord bot!
--- bracketcounterrevamped/__main__.py
+++ bracketcounterrevamped/__main__.py
@@ -1,1 +1,22 @@
-print("Hello, world!")
+from dotenv import load_dotenv
+
+import nextcord
+from nextcord.ext import commands
+
+import os
+
+load_dotenv()
+
+TESTING_GUILD_ID = 835494729554460702
+
+bot = commands.Bot()
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.slash_command(description="My first slash command", guild_ids=[TESTING_GUILD_ID])
+async def hello(interaction: nextcord.Interaction):
+    await interaction.send("Hello!")
+
+bot.run(os.getenv("DISCORD_TOKEN"))
