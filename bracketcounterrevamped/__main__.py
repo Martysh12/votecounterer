c8be15931a79968d62d10e6325da29a15ed33eb6
Martin K <martyshkon@gmail.com>
2023-06-07 00:07:24 +0300

    Set up logging

--- bracketcounterrevamped/__main__.py
+++ bracketcounterrevamped/__main__.py
@@ -3,9 +3,30 @@ from dotenv import load_dotenv
 import nextcord
 from nextcord.ext import commands
 
+import logging
 import os
 
+# Load environment variables from ".env"
 load_dotenv()
+
+# Set up logging
+logger = logging.getLogger('votecounterer')
+logger.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler()
+fh = logging.FileHandler('votecounterer.log')
+
+ch.setLevel(logging.DEBUG)
+fh.setLevel(logging.INFO)
+
+ch_formatter = logging.Formatter('%(levelname)-8s :: %(message)s')
+fh_formatter = logging.Formatter('%(name)s / %(asctime)-23s :: %(levelname)-8s :: %(message)s')
+
+ch.setFormatter(ch_formatter)
+fh.setFormatter(fh_formatter)
+
+logger.addHandler(ch)
+logger.addHandler(fh)
 
 TESTING_GUILD_ID = 835494729554460702
 
@@ -13,10 +34,6 @@ bot = commands.Bot()
 
 @bot.event
 async def on_ready():
-    print(f'We have logged in as {bot.user}')
+    logger.info(f"Logged in as {bot.user}")
 
-@bot.slash_command(description="My first slash command", guild_ids=[TESTING_GUILD_ID])
-async def hello(interaction: nextcord.Interaction):
-    await interaction.send("Hello!")
-
-bot.run(os.getenv("DISCORD_TOKEN"))
+bot.run(os.getenv('DISCORD_TOKEN'))
